60608771f4bb0d4a9bc7f8d9bd9da8b81eff213b
andersoncorreia <anderson-strife@outlook.com>
2022-02-12 13:21:03 -0300

    function make in ProductStructure, test ok

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -20,7 +20,23 @@ class ProductStructure
 
     public function make(): array
     {
-       //todo your code.
-        return [];
+        $productsStandardized  = [];
+
+        foreach( self::products as $productString ) {
+            $product = explode("-", $productString);
+            $productColor = $product[0];
+            $productSize = $product[1];
+            if( !isset($productsStandardized[$productColor]) ) {
+                $productsStandardized[$productColor] = [];
+            }
+            // increment by 1 if the size has already been defined or initialize it to 1
+            if( isset($productsStandardized[$productColor][$productSize]) ) {
+                $productsStandardized[$productColor][$productSize]++;
+            }
+            else {
+                $productsStandardized[$productColor][$productSize] = 1;
+            }
+        }
+        return $productsStandardized;
     }
 }
